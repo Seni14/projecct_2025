98da9aa2be34defdad763e5ae838e5cda2837cf1
seni14 <seni@abv.bg>
2025-01-18 14:38:28 +0200

    Initial commit

---
+++ src/test/java/com/softuni/project_2025/Project2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softuni.project_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
